0ccfd7f5aa90a32cbdf2aaf1d91b652e4e75e63b
Filipe <filiperdacas@gmail.com>
2024-06-12 17:18:18 -0300

    fixed bugs

--- src/main/java/com/App_Service_Back/endereco/EnderecoController.java
+++ src/main/java/com/App_Service_Back/endereco/EnderecoController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController //habilitar o REST
 @RequestMapping("/endereco") // localhost:8080/endereco
 public class EnderecoController {
